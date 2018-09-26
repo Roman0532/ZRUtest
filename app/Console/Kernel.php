795f59be69e37c6a4170b20169b4e6cd797cbb15
roman0532 <roman62477@yandex.ru>
2018-09-26 18:57:21 +0700

    Рефакторинг

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('transactions:begin')->everyMinute();
+        $schedule->command('transactions:begin')->hourly();
         $schedule->command('sms:send')->daily();
     }
 
